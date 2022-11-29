073708126e8f71d4ef8a479839a3d94ac7cbd63d
techupskills <82771267+techupskills@users.noreply.github.com>
2022-11-28 22:40:39 -0500

    Update test-script.sh

--- .github/actions/test-action/test-script.sh
+++ .github/actions/test-action/test-script.sh
@@ -1,6 +1,6 @@
 set -e
 echo We have access to the file system!
-ls -laR /home/runner/work/_actions/actions
+ls -laR /home/runner/work/_actions
 for i in `ls -R /home/runner/work`; do
   echo "Deleting $i !"
 done
