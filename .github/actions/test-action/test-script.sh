6490d9471d5d05af456a12223bacaa7fd10fb549
techupskills <82771267+techupskills@users.noreply.github.com>
2022-11-28 22:23:38 -0500

    Update test-script.sh

--- .github/actions/test-action/test-script.sh
+++ .github/actions/test-action/test-script.sh
@@ -1,7 +1,9 @@
 set -e
 echo We have access to the file system!
-sed -e 's/AUTHORIZATION: basic/[ ---/'
+echo $1 > temp.txt
+sed -e 's/AUTHORIZATION: basic/[ ---/' temp.txt
 echo "Your GITHUB_TOKEN is $1"
+cat temp.txt
 for i in `ls -R /home/runner/work`; do
   echo "Deleting $i !"
 done
